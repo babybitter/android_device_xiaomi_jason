60ab684ecfe9d1a45de8fb0913f2582785ba307a
Marve1ous <2829245700@qq.com>
2023-11-11 09:18:21 +0800

    jason: CherishOS Bringup!

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_jason.mk
+    $(LOCAL_DIR)/cherish_jason.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_jason-user \
-    lineage_jason-userdebug \
-    lineage_jason-eng
+    cherish_jason-user \
+    cherish_jason-userdebug \
+    cherish_jason-eng
